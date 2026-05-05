5e5fe8eded403b925707c65f528aae192f2f82dd
buddystudiostudio <buddyenglishstudio@gmail.com>
2026-05-05 16:57:08 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{textAlign:'center'}}>
+      <h1>Buddy English Studio</h1>
+      <p>Welcome!</p>
+      <img src="/profile.png" width="300" />
+    </div>
+  )
+}
