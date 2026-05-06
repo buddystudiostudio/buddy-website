ff09a0f23bb0ceb809cf18a3836f14115c575ce9
buddystudiostudio <buddyenglishstudio@gmail.com>
2026-05-06 13:11:02 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './style.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./style.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import BuddyEnglishStudioWebsite from "./App.jsx";
+import Blog from "./Blog.jsx";
+import BlogPost from "./BlogPost.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BuddyEnglishStudioWebsite />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
